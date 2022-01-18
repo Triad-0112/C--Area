ad727cfca2393fef4eab536bf1aece73b0de0d65
Panji Tri Wahyudi <richard.kennedy11316@gmail.com>
2022-01-18 11:36:14 +0700

    Faktorisasi Prima

---
+++ TrainingGround/Math/PrimeFactorNumber/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <iterator>
+std::vector<unsigned long long> prime_factors(unsigned long long n) {
+    std::vector<unsigned long long> factors;
+    while (n % 2 == 0) {
+        factors.push_back(2);
+        n = n / 2;
+    }
+    for (unsigned long long i = 3; i <= std::sqrt(n); i+=2) {
+        while (n%i == 0) {
+            factors.push_back(i);
+            n = n / i;
+        }
+    }
+    if (n > 2) {
+        factors.push_back(n);
+    }
+    return factors;
+}
+int printPrimeFactorNumber(int const number) {
+    auto factors = prime_factors(number);
+    std::copy(std::begin(factors),std::end(factors),
+        std::ostream_iterator<unsigned long long>(std::cout, "  "));
+    return 0;
+}
+int main() {
+    unsigned long long number = 0;
+    std::cout << "Number: ";
+    std::cin >> number;
+    printPrimeFactorNumber(number);
+}
+
